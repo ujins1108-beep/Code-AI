e5c4926f169047b0f838069323b17926d35463cc
XMFL <ujins1108@gmail.com>
2026-06-03 06:52:52 +0000

    히스토리 추가 수정 4

--- script.js
+++ script.js
@@ -61,9 +61,7 @@ async function groqfetch() {
 
             body: JSON.stringify({
                 model: "llama-3.1-8b-instant",
-                messages: [
-                    { role: "user", content: history }
-                ]
+                messages: history
             })
         }
     )
